2c19d9468ec8a95782f549d2acd504fdb38ade9c
talhaozturk07 <talha.ozturk@itpoint.com.tr>
2025-05-12 12:37:53 +0000

    İlk commit: Temiz başlangıç

--- app/Http/Kernel.php
+++
@@ -1,6 +0,0 @@
-// Kernel.php içinde protected $routeMiddleware array'ine ekleyin
-protected $routeMiddleware = [
-    // Mevcut middleware'ler...
-    'role' => \App\Http\Middleware\CheckRole::class,
-    'permission' => \App\Http\Middleware\CheckPermission::class,
-];
